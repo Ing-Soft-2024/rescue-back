45e8fa9c3c0554f46aa569a74bd1d487e8de6502
juane993 <jolivamorroni@itba.edu.ar>
2024-10-26 20:56:19 -0300

    category test

--- tests/category/category.test.js
+++ tests/category/category.test.js
@@ -1,27 +1,37 @@
-// // Using babel-jest
+const CategoryMock = jest.mock("database/models/category.model", () => {
+    // Initialize SequelizeMock and define the CategoryMock model within the jest.mock scope
+    const SequelizeMock = require('sequelize-mock');
+    const dbMock = new SequelizeMock();
+    const category=  dbMock.define('Category', {
+        id: 1,
+        name: 'Comida',
+        description: 'Comida'
+    });
+    
+    return category;
+});
+  
 
-// const { getCategoryById } = require("app/categories/[id]/controller/category.get");
-// const { getCategories } = require("app/categories/controller/categories.get");
-// const { default: Category } = require("database/models/category.model");
+// Import your functions after defining the mock
+const { getCategories } = require("app/categories/controller/categories.get");
+const { getCategoryById } = require("app/categories/[id]/controller/category.get");
 
-// test('Get categories', async () => {
-//     await getCategories()
-//         .then((categories) => {
-//             expect(categories instanceof Array).toBe(true);
-//             expect(categories instanceof Category).toBe(false);
-//             if (categories.length > 0) expect(categories[0] instanceof Category).toBe(true);
+test('Get categories', async () => {
+    await getCategories()
+        .then((categories) => {
+            expect(categories instanceof Array).toBe(true);
+            expect(categories.length).toBeGreaterThan(0);
+            expect(categories[0].name).toBe('Comida');
+            console.log("Categories retrieved successfully");
+            console.log(categories);
+        })
+        .catch((err) => {
+            expect(err).toBe("Error while fetching categories");
+        });
+});
 
-//             console.log("Categorías obtenidas correctamente");
-//         })
-//         .catch((err) => {
-//             expect(err).toBe("Error al obtener las categorías");
-//         })
-
-// })
-
-// test('Get category by id', async () => {
-//     const category = await getCategoryById(1);
-//     expect(category instanceof Category).toBe(true);
-//     expect(category.name).toBe('Comida');
-//     expect(category.description).toBe('Comida');
-// })
+test('Get category by id', async () => {
+    const category = await getCategoryById(1);
+    expect(category.name).toBe('Comida');
+    expect(category.description).toBe('Comida');
+});
