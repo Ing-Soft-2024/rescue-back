37ab7791521aa7b743f663094105b1a2ab527edb
Nicolas Kim <nkim@itba.edu.ar>
2024-10-31 19:47:10 -0300

    Categories in router.js

--- src/app/categories/controller/categories.get.js
+++ src/app/categories/controller/categories.get.js
@@ -1,8 +1,11 @@
 import Category from "database/models/category.model";
+import Product from "database/models/product.model";
 
 export const getCategories = async () => {
     // throw new Error("Error al obtener las categorías");
-    const categories = await Category.findAll()
+    const categories = await Category.findAll({
+        includes: [{ model: Product }]
+    })
         .catch((err) => {
             console.error(err);
             throw new Error("Error al obtener las categorías");
